8b1b2a93f5fd52f94f3369b7434c37e392cd83e8
Paul Shannon <paul@paulshannon.com>
2012-01-15 22:13:15 +0000

    refactored into classes

--- Blinky/Program.cs
+++ Blinky/Program.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Threading;
-using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
-using SecretLabs.NETMF.Hardware;
 using SecretLabs.NETMF.Hardware.Netduino;
 
-/* NOTE: make sure you change the deployment target from the Emulator to your Netduino before running this
- * Netduino sample app.  To do this, select "Project menu > Blinky Properties > .NET Micro Framework" and 
- * then change the Transport type to USB.  Finally, close the Blinky properties tab to save these settings. */
 
 namespace Blinky
 {
@@ -15,48 +9,102 @@ namespace Blinky
     {
         public static void Main()
         {
-            // write your code here
-            var ledPort = new OutputPort(Pins.ONBOARD_LED, false);
+            var led = new OutputPort(Pins.ONBOARD_LED, false);
+            var breather = new LedBreather(5, led, new Breather(new LoopDimmer()));
+            breather.Run();
+        }       
+    }
 
-            int iMax = 0xff, iMin = 1;
-            int i = iMin;
+    public class LedBreather
+    {
+        private readonly int _maxCycles;
+        private readonly OutputPort _led;
+        private readonly IBreather _breather;
 
+        public LedBreather(int maxCycles, OutputPort led, IBreather breather)
+        {
+            _maxCycles = maxCycles;
+            _led = led;
+            _breather = breather;
+        }
+
+        public void Run()
+        {
             var cycles = 0;
-            var MAX_CYCLES = 5;
-            while (cycles < MAX_CYCLES)
+            while (cycles++ < _maxCycles)
+                _breather.Breathe(_led);
+        }
+    }
+
+    public interface IBreather
+    {
+        void Breathe(OutputPort led);
+    }
+
+    public class Breather : IBreather
+    {
+        private readonly IDimmer _dimmer;
+
+        public Breather(IDimmer dimmer)
+        {
+            _dimmer = dimmer;
+        }
+
+        public  void Breathe(OutputPort led)
+        {
+            _dimmer.FadeIn(led);
+            _dimmer.FadeOut(led);
+        }
+    }
+
+    public class LoopDimmer : Dimmer
+    {
+        public override void FadeOut(OutputPort ledPort)
+        {
+            for (var j = MAX_BRIGHTNESS; j > MIN_BRIGHTNESS; j--)
             {
-                for (; i < iMax; i++)
-                {
+                ledPort.Write(true);
+                DelayFor(j);
 
-                    ledPort.Write(true);
-                    funDelay(i);
-
-                    ledPort.Write(false);
-                    funDelay(iMax - i);
-
-                }
-
-                for (; i > iMin; i--)
-                {
-
-                    ledPort.Write(true);
-                    funDelay(i);
-
-                    ledPort.Write(false);
-                    funDelay(iMax - i);
-
-                }
-                cycles++;
+                ledPort.Write(false);
+                DelayFor(MAX_BRIGHTNESS - j);
             }
         }
 
-        static private void funDelay(long lTicks)
+        public override void FadeIn(OutputPort ledPort)
         {
-            DateTime tNow = DateTime.Now;
-            DateTime tDue = tNow.AddTicks(lTicks * 0x100);
-            while (DateTime.Now < tDue) ;
+            for (var i = MIN_BRIGHTNESS; i < MAX_BRIGHTNESS; i++)
+            {
+                ledPort.Write(true);
+                DelayFor(i);
+
+                ledPort.Write(false);
+                DelayFor(MAX_BRIGHTNESS - i);
+            }
+        }
+    }
+
+    public abstract class Dimmer : IDimmer
+    {
+        public const int MAX_BRIGHTNESS = 0xff;
+        public const int MIN_BRIGHTNESS = 1;
+
+        public static void DelayFor(long ticks)
+        {
+            var now = DateTime.Now;
+            var due = now.AddTicks(ticks * 0x100);
+            while (DateTime.Now < due) ;
 
             return;
         }
+
+        public abstract void FadeIn(OutputPort ledPort);
+        public abstract void FadeOut(OutputPort ledPort);
+    }
+
+    public interface IDimmer
+    {
+        void FadeIn(OutputPort ledPort);
+        void FadeOut(OutputPort ledPort);
     }
 }
